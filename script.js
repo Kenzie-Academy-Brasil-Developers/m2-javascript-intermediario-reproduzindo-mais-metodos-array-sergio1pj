ca6680167f5db817eadd49bb93a1374428ac271c
Sergio Pereira <72481029+sergio1pj@users.noreply.github.com>
2022-07-27 21:05:25 -0300

    explicitando que foreach tem return undefined, assim como o método de mesmo nome

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ function forEach(array, callback) {
   for(let i = 0; i < array.length; i++) {
     callback(array[i]);
   }
+  return undefined;
 }
 
 console.log(forEach(arrayForEach, callbackForEach));
